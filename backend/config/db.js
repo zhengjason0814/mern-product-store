d53ba39a351dfcf3dde5e4b93d15c6cc6a2444ce
Jason Zheng <jasonzheng0814@gmail.com>
2025-07-15 02:22:56 -0400

    add console connection message

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("Connected DB:", mongoose.connection.name);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1); // 0 success 1 failure
